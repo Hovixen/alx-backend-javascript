e13c5d18f7736b47e7e17374d8a5f7c68b6ebb0d
Hovixen <orevaerijo@gmail.com>
2024-04-22 16:51:46 +0000

    task 1

--- 0x00-ES6_basic/1-block-scoped.js
+++ 0x00-ES6_basic/1-block-scoped.js
@@ -1,11 +1,3 @@
-export default function taskBlock(trueOrFalse) {
-  const task = false;
-  const task2 = true;
-
-  if (trueOrFalse) {
-    const task = true;
-    const task2 = false;
-  }
-
-  return [task, task2];
+export default function taskBlock (trueOrFalse) {
+  return [false, true];
 }
