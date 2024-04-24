9fec884ce04f18768c0a181f064aa909778a0267
Hovixen <orevaerijo@gmail.com>
2024-04-24 13:03:31 +0000

    task 14

--- 0x00-ES6_basic/101-iterateThroughObject.js
+++ 0x00-ES6_basic/101-iterateThroughObject.js
@@ -1,10 +1,3 @@
 export default function iterateThroughObject(reportWithIterator) {
-  let employeeString = '';
-
-  for (const employee of reportWithIterator) {
-    employeeString += ` ${employee} |`;
-  }
-  employeeString = employeeString.slice(0, -1);
-
-  return employeeString;
+  return reportWithIterator.join(' | ');
 }
