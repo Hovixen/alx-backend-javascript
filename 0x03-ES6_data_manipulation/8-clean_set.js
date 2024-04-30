421641880a9e47df199c8c3af8582aedec78a59a
Hovixen <orevaerijo@gmail.com>
2024-04-30 07:06:19 +0000

    task 8

---
+++ 0x03-ES6_data_manipulation/8-clean_set.js
@@ -0,0 +1,13 @@
+export default function cleanSet(set, string) {
+  if (string.length === 0) {
+    return '';
+  }
+  let subString = '';
+
+  for (const element of set) {
+    if (element.startsWith(string)) {
+      subString += `${element.slice(string.length)}-`;
+    }
+  }
+  return subString.slice(0, -1);
+}
