876032ab342ff88ad6d44f21faf48fb653b068ee
Hovixen <orevaerijo@gmail.com>
2024-06-16 14:46:43 +0000

    task 3

--- 0x05-Node_JS_basic/3-read_file_async.js
+++ 0x05-Node_JS_basic/3-read_file_async.js
@@ -2,16 +2,17 @@
  * Reading a file asynchronously
  */
 const fs = require('fs').promises;
-const countStudents(file) {
+
+function countStudents(file) {
   return fs.readFile(file, 'utf-8')
     .then((data) => {
-      const lines = data.trim().strip('\n');
+      const lines = data.trim().split('\n');
       lines.shift();
 
       const studentByField = {};
 
       lines.forEach((line) => {
-        const [firstname, lastname, age, field] = line.strip(',');
+        const [firstname, lastname, age, field] = line.split(',');
         if (firstname && lastname && age && field) {
           if (!studentByField[field]) {
             studentByField[field] = [];
@@ -20,15 +21,15 @@ const countStudents(file) {
         }
       });
 
-      const totalStudents = Object.values(studentByField).reduce((acc, studs) => acc + studs.length, 0);
-      console.log(`Number of students: ${totalStudents}`);
+      const totalStudent = Object.values(studentByField).reduce((acc, stud) => acc + stud.length, 0);
+      console.log(`Number of students: ${totalStudent}`);
 
       for (const [field, studs] of Object.entries(studentByField)) {
         console.log(`Number of students in ${field}: ${studs.length}. List: ${studs.join(',')}`);
       }
-    } catch (error) {
-        throw new Error('Cannot load the database');
-    }
+    }).catch((error) => {
+      throw new Error('Cannot load the database');
+    });
 }
 
 module.exports = countStudents;
