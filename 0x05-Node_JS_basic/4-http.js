661298ed8270fcc4ca8859ec271b40ba1612a60c
Hovixen <orevaerijo@gmail.com>
2024-06-16 16:15:53 +0000

    task 4

---
+++ 0x05-Node_JS_basic/4-http.js
@@ -0,0 +1,18 @@
+/*
+ * small http server using Node's http module
+ */
+const http = require('http');
+
+const port = 1245;
+
+const app = http.createServer((req, res) => {
+  res.writeHead(200, { 'Content-Type': 'text/plain' });
+
+  res.end('Hello Holberton School!');
+});
+
+app.listen(port, () => {
+  console.log(`Server is listening on port: ${port}`);
+});
+
+module.exports = app;
