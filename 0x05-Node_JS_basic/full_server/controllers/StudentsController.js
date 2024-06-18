c1da86f4638fb66c5702e70f433b814c239e087a
Hovixen <orevaerijo@gmail.com>
2024-06-18 12:55:15 +0000

    task 8

--- 0x05-Node_JS_basic/full_server/controllers/StudentsController.js
+++ 0x05-Node_JS_basic/full_server/controllers/StudentsController.js
@@ -6,24 +6,25 @@ import readDatabase from '../utils';
 class StudentsController {
   static async getAllStudents(request, response) {
     const studentData = process.argv[2];
-    if (studentData) {
-      try {
-        const data = await readDatabase(studentData);
-        const fields = Object.keys(data).sort((a, b) => {
-          a.toLowerCase().localeCompare(b.toLowerCase());
-        });
 
-        const output = fields.map((field) => {
-          const students = data[field]
-          return `Number of students in ${field}: ${data[field].length}. List: ${students.join(',' )}\n`;
-        });
-        response.statusCode = 200;
-        response.send(`This is the list of our students\n${output}`);
-      } catch (error) {
-        response.status(500).send('Cannot load the database');
-      }
-    } else {
+    if (!studentData) {
       response.status(500).send('No database provided');
+      return;
+    }
+
+    try {
+      const data = await readDatabase(studentData);
+      const fields = Object.keys(data).sort((a, b) => a.localeCompare(b, undefined, { sensitivity: 'base' }));
+
+      let output = 'This is the list of our students\n';
+      fields.forEach((field) => {
+        const students = data[field].join(', ');
+        output += `Number of students in ${field}: ${data[field].length}. List: ${students}\n`;
+      });
+
+      response.status(200).send(output.trim());
+    } catch (error) {
+      response.status(500).send('Cannot load the database');
     }
   }
 
