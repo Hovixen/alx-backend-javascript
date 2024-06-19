65da3821efd39ceb640e47df6cd15782352cd313
Hovixen <orevaerijo@gmail.com>
2024-06-19 17:57:22 +0000

    task 0

---
+++ 0x06-unittests_in_js/0-calcul.js
@@ -0,0 +1,9 @@
+/*
+ * function calculate number and round the sum of the argument
+ */
+
+function calculateNumber(a,b) {
+  return Math.round(a) + Math.round(b);
+}
+
+module.exports = calculateNumber;
