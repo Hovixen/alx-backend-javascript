11d0f27a859d490f470191fd5907a99b15610a41
Hovixen <orevaerijo@gmail.com>
2024-06-19 18:51:32 +0000

    task 0

--- 0x06-unittests_in_js/0-calcul.test.js
+++ 0x06-unittests_in_js/0-calcul.test.js
@@ -21,4 +21,18 @@ describe('calculateNumber', () => {
   it('should return 6 when inputs are 1.5 and 3.7', () => {
     assert.strictEqual(calculateNumber(1.5, 3.7), 6);
   });
+  it('should return 1 when inputs are -1.5 and 1.5', () => {
+    assert.strictEqual(calculateNumber(-1.5, 1.5), 1);
+  });
+  it('should return 1 when inputs are -1.4 and -1.5', () => {
+    assert.strictEqual(calculateNumber(-1.4, 1.5), 1);
+  });
+
+  it('should return 2 when inputs are 0.5 and 0.5', () => {
+    assert.strictEqual(calculateNumber(0.5, 0.5), 2);
+  });
+
+  it('should return 0 when inputs are 0 and 0', () => {
+    assert.strictEqual(calculateNumber(0, 0), 0);
+  });
 });
