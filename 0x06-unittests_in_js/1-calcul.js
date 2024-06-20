9302f614c85bbd8a582c7c64d5386787c21754dc
Hovixen <orevaerijo@gmail.com>
2024-06-20 14:19:21 +0000

    no arrow function

---
+++ 0x06-unittests_in_js/1-calcul.js
@@ -0,0 +1,24 @@
+/*
+ * upgraded function to calculate number by adding description
+ */
+
+function calculateNumber(type, a, b) {
+  const Anum = Math.round(a);
+  const Bnum = Math.round(b);
+
+  switch(type) {
+    case 'SUM':
+      return Anum + Bnum;
+    case 'SUBTRACT':
+      return Anum - Bnum;
+    case 'DIVIDE':
+      if (Bnum === 0) {
+        return 'Error';
+      }
+      return Anum / Bnum;
+    default:
+      throw new Error('Invalid operation type');
+  }
+}
+
+module.exports = calculateNumber;
---
+++ 0x06-unittests_in_js/1-calcul.test.js
@@ -0,0 +1,14 @@
+/*
+ * Test case for 1-calcul.js
+ */
+
+const assert = require('assert');
+const calculateNumber = require('./1-calcul');
+
+describe('calculateNumber', () => {
+  describe('SUM', () => {
+    it('should return 6 when inputs are 1.4 and 4.5' {
+      assert.equal(calculateNumber('SUM',(1.4, 4.5),6);
+    }
+
+
